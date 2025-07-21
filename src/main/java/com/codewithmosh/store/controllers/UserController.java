e312a39dcaaebd37dba69ee98f51b68bd1bb53af
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-07-22 01:16:42 +0700

    Create /users/id endpoint, implement custom HTTP response codes

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -2,7 +2,11 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,15 @@ public class UserController {
     @GetMapping("/users")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null) {
+            ///  return a new response entity with status not found
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
