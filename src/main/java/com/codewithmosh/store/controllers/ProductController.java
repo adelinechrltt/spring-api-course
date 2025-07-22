94571c81c3f8bb0f9219ee401abfab0e16e7f693
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-07-23 00:49:45 +0700

    Add Product DTO

---
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ProductController {
+    private final ProductRepository productRepository;
+}
+
