bf4148b346cc08aa35420f9730deb82272f6923a
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-07-22 23:44:14 +0700

    Add @Mapping function to UserMapper

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,12 +1,20 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonIgnore
     private Long id;
     private String name;
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNumber;
+    private LocalDateTime createdAt;
 }
