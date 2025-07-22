c38f4d5bcf879d3e6d70949aac2396011cafed8e
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-07-23 00:50:47 +0700

    Add Product mapper

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -8,5 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class ProductController {
     private final ProductRepository productRepository;
+
 }
 
---
+++ src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.mappers;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    Product toProduct(ProductDto dto);
+}
