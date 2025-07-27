339b94441c96d76edb2480a0be15ace0ba809074
Adeline Charlotte <adeline.chrltt@gmail.com>
2025-07-27 13:25:14 +0700

    Add CREATE and UPDATE product endpoints

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -1,12 +1,15 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.mappers.ProductMapper;
 import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Set;
@@ -41,5 +44,36 @@ public class ProductController {
     public ProductDto getProduct(@PathVariable Long id){
         return productMapper.toDto(productRepository.findById(id).get());
     }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto request,
+            UriComponentsBuilder builder
+    ){
+        var product = productMapper.toEntity(request);
+        productRepository.save(product);
+
+        var dto = productMapper.toDto(product);
+        var uri = builder.path("/products/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable(name = "id") Long id,
+            @RequestBody ProductDto request
+    ){
+        var product = productRepository.findById(id).orElse(null);
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productMapper.update(request, product);
+        productRepository.save(product);
+
+        return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+
 }
 
--- src/main/java/com/codewithmosh/store/dtos/ProductDto.java
+++ src/main/java/com/codewithmosh/store/dtos/ProductDto.java
@@ -1,12 +1,15 @@
 package com.codewithmosh.store.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@NoArgsConstructor
 @AllArgsConstructor
-@Getter
+@Data
 public class ProductDto {
     private Long id;
     private String name;
